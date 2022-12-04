83b8483bfcb9a6fdf8058fef66972098274ab059
Eduardo Gonzalez <edgoze@wharton.upenn.edu>
2022-12-04 14:28:24 -0500

    frontend routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import { Home } from "./components";
+import { Home, Recommendations, Search } from "./components";
 import "./App.css";
 
 function App() {
@@ -8,6 +8,8 @@ function App() {
     <Router>
       <Routes>
         <Route exact path="/" element={<Home />} />
+        <Route exact path="/search" element={<Search />} />
+        <Route exact path="/recommendation" element={<Recommendations />} />
       </Routes>
     </Router>
   );
