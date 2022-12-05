f06f7a3b1e59241f13f023813cedcbd0cc149508
Jeffrey Xiao <jeffrey.xiao1@gmail.com>
2022-12-05 13:46:37 -0500

    add detailed view to search

--- client/src/components/Search.js
+++ client/src/components/Search.js
@@ -1,82 +1,106 @@
 import React, { useState, useEffect } from "react";
-import { Input, Table } from 'antd';
-import {getSearch} from '../modules/api';
+import { Input, Table, Modal } from "antd";
+import DetailedView from "./DetailedView";
+import { getSearch } from "../modules/api";
 
 function Search() {
-  const [searchText, setSearchText] = useState('');
+  const [searchText, setSearchText] = useState("");
   const [data, setData] = useState(null);
+  const [detailedViewItem, setDetailedViewItem] = useState(null);
   const handleSearch = (e) => {
     setSearchText(e.target.value);
   };
 
-
   useEffect(() => {
     const fetchResults = async () => {
-        const results = await getSearch({
+      const results = await getSearch({
         search: searchText,
-        });
-        console.log("ran search");
-        const newArr = []
-        for (let i = 0; i < results.data.results.length; i++) {
-            newArr[i] = results.data.results[i] 
-            newArr[i].key = i 
-        }
-        setData(newArr);
+      });
+      console.log("ran search");
+      const newArr = results.data.results.map((item, i) => ({
+        ...item,
+        key: i,
+      }));
+      setData(newArr);
+      console.log(newArr);
     };
-     if (searchText !== "") {
-        fetchResults();
-     }
+    if (searchText !== "") {
+      console.log("running");
+      fetchResults();
+    }
   }, [searchText]);
-
 
   const columns = [
     {
-      title: 'Title',
-      dataIndex: 'Title',
-      key: 'Title',
-      width: '30%',
+      title: "Title",
+      dataIndex: "Title",
+      key: "Title",
+      width: "30%",
     },
     {
-      title: 'Type',
-      dataIndex: 'Type',
-      key: 'Type',
-      width: '20%',
+      title: "Type",
+      dataIndex: "Type",
+      key: "Type",
+      width: "20%",
       filters: [
         {
-          text: 'book',
-          value: 'book',
+          text: "book",
+          value: "book",
         },
         {
-          text: 'movie',
-          value: 'movie',
+          text: "movie",
+          value: "movie",
         },
       ],
       onFilter: (value, record) => record.Type.includes(value),
       filterSearch: true,
     },
     {
-      title: 'Rating',
-      dataIndex: 'rating',
-      key: 'rating',
-      width: '20%',
+      title: "Rating",
+      dataIndex: "rating",
+      key: "rating",
+      width: "20%",
       sorter: (a, b) => a.rating - b.rating,
-      sortDirections: ['descend', 'ascend'],
+      sortDirections: ["descend", "ascend"],
     },
     {
-      title: 'Genres',
-      dataIndex: 'GenreList',
-      key: 'GenreList',
+      title: "Genres",
+      dataIndex: "GenreList",
+      key: "GenreList",
     },
   ];
 
   return (
     <div>
-      <Input placeholder="Type in a title, author, director, or actor, and press enter to search" onPressEnter={handleSearch}/>
-      <Table columns={columns} dataSource={data} />
+      <Input
+        placeholder="Type in a title, author, director, or actor, and press enter to search"
+        onPressEnter={handleSearch}
+      />
+      <Table
+        onRow={(record) => {
+          return {
+            onClick: () => {
+              setDetailedViewItem(record);
+            }, // click row
+          };
+        }}
+        columns={columns}
+        dataSource={data}
+      />
+      <Modal
+        open={detailedViewItem !== null && detailedViewItem !== undefined}
+        onOk={() => setDetailedViewItem(null)}
+        onCancel={() => setDetailedViewItem(null)}
+        footer={null}
+        width={1000}
+      >
+        <DetailedView
+          id={detailedViewItem && detailedViewItem.Id}
+          isBook={detailedViewItem && detailedViewItem.Type === "book"}
+        />
+      </Modal>
     </div>
   );
 }
 
 export default Search;
-
-
