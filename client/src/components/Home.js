0ae30b04e301de756c71133613ab12c5214dac41
Jeffrey Xiao <55001755+jxiao@users.noreply.github.com>
2022-12-05 19:08:56 -0500

    Basic Navbar (#7)

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,55 +1,56 @@
-import React, { useState, useEffect } from 'react';
-import { Carousel, Avatar, List } from 'antd';
-import { bestDirector, getBooks, getMovies } from '../modules/api'
+import React, { useState, useEffect } from "react";
+import { Carousel, Avatar, List } from "antd";
+import { bestDirector, getBooks, getMovies } from "../modules/api";
+import { Navbar } from "./index.js";
 
 const styles = {
-    box: {
-        width: "35%",
-        margin: "auto",
-        boxShadow: "2px 2px 10px rgba(0,0,0,.2)",
-        padding: "45px 20px",
-        borderRadius: "15px",
-        position: "absolute",
-        top: "30%",
-        left: "50%",
-        transform: "translate(-50%, -30%)",
-        zIndex: "2",
-        background: "white"
-    },
-    flex: {
-        display: "flex",
-        flexDirection: "column",
-        justifyContent: "center",
-        margin: "auto",      
-    },
-    input: {
-        borderColor: "grey",
-    },
-    contentStyle: {
-      height: '160px',
-      color: '#fff',
-      lineHeight: '160px',
-      textAlign: 'center',
-      background: '#364d79',
-    },
-    listItem: {
-      display: 'flex',
-      alignItems: 'center'
-    }
-}
+  box: {
+    width: "35%",
+    margin: "auto",
+    boxShadow: "2px 2px 10px rgba(0,0,0,.2)",
+    padding: "45px 20px",
+    borderRadius: "15px",
+    position: "absolute",
+    top: "30%",
+    left: "50%",
+    transform: "translate(-50%, -30%)",
+    zIndex: "2",
+    background: "white",
+  },
+  flex: {
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center",
+    margin: "auto",
+  },
+  input: {
+    borderColor: "grey",
+  },
+  contentStyle: {
+    height: "160px",
+    color: "#fff",
+    lineHeight: "160px",
+    textAlign: "center",
+    background: "#364d79",
+  },
+  listItem: {
+    display: "flex",
+    alignItems: "center",
+  },
+};
 
 const listData = [
   {
-    title: 'Ant Design Title 1',
+    title: "Ant Design Title 1",
   },
   {
-    title: 'Ant Design Title 2',
+    title: "Ant Design Title 2",
   },
   {
-    title: 'Ant Design Title 3',
+    title: "Ant Design Title 3",
   },
   {
-    title: 'Ant Design Title 4',
+    title: "Ant Design Title 4",
   },
 ];
 
@@ -59,13 +60,13 @@ function Home() {
   const [movieData, setMovieData] = useState();
   useEffect(() => {
     async function fetchData() {
-      const {status, data} = await bestDirector();
+      const { status, data } = await bestDirector();
       console.log(data);
       setBestData(data);
-      const {status: bookStatus, data: bookData} = await getBooks();
+      const { status: bookStatus, data: bookData } = await getBooks();
       console.log(bookData);
       setBookData(bookData);
-      const {status: movieStatus, data: movieData} = await getMovies();
+      const { status: movieStatus, data: movieData } = await getMovies();
       console.log(movieData);
       setMovieData(movieData);
     }
@@ -74,7 +75,8 @@ function Home() {
 
   return (
     <div>
-      <h1 style={{textAlign: 'center'}}>Entertainment Engine</h1> 
+      <Navbar />
+      <h1 style={{ textAlign: "center" }}>Entertainment Engine</h1>
       <Carousel autoplay>
         <div>
           <h3 style={styles.contentStyle}>A movie</h3>
@@ -93,8 +95,9 @@ function Home() {
         itemLayout="horizontal"
         dataSource={listData}
         renderItem={(item) => (
-          <List.Item >
-            <List.Item.Meta style={styles.listItem}
+          <List.Item>
+            <List.Item.Meta
+              style={styles.listItem}
               avatar={<Avatar src="https://joeschmoe.io/api/v1/random" />}
               title={<a href="https://ant.design">{item.title}</a>}
               description="Ant Design, a design language for background applications, is refined by Ant UED Team"
